63b8ae0fd4d604eae8287b5f7fa6cf43cacdcc92
NoloX24 <nolan.pruett@gmail.com>
2016-12-27 05:09:50 -0500

    Added existing projects to the solution

---
+++ JapaneseWorld/JapaneseWorld/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JapaneseLearningApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Japanese World: A web application for learning the Japanese language";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Let me know how I am doing!";
+
+            return View();
+        }
+    }
+}
---
+++ JapaneseWorld/JapaneseWorld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JapaneseLearningApp.MvcApplication" Language="C#" %>
